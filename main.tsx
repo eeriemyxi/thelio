abfab399838704dd8d5304ef087e9f560cdd38c4
myxi <myxi@envs.net>
2024-10-17 13:52:22 +0530

    fix: fmt, lint

--- main.tsx
+++ main.tsx
@@ -1,34 +1,34 @@
-import { Hono } from '@hono/hono'
-import type { FC } from '@hono/hono/jsx'
+import { Hono } from "@hono/hono";
+import type { FC } from "@hono/hono/jsx";
 
-const app = new Hono()
+const app = new Hono();
 
 const Layout: FC = (props) => {
   return (
     <html>
       <body>{props.children}</body>
     </html>
-  )
-}
+  );
+};
 
 const Top: FC<{ messages: string[] }> = (props: {
-  messages: string[]
+  messages: string[];
 }) => {
   return (
     <Layout>
       <h1>Hello Hono!</h1>
       <ul>
         {props.messages.map((message) => {
-          return <li>{message}!!</li>
+          return <li>{message}!!</li>;
         })}
       </ul>
     </Layout>
-  )
-}
+  );
+};
 
-app.get('/', (c) => {
-  const messages = ['Good Morning', 'Good Evening', 'Good Night']
-  return c.html(<Top messages={messages} />)
-})
+app.get("/", (c) => {
+  const messages = ["Good Morning", "Good Evening", "Good Night"];
+  return c.html(<Top messages={messages} />);
+});
 
 Deno.serve(app.fetch);
